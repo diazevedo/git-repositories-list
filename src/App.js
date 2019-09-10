83f79464060be43fd4b5045e344ab343973d4b2c
Diego Azevedo <dapptn@hotmail.com>
2019-09-10 17:13:52 +1000

    reset css created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import Routes from './routes';
+import ResetCSS from './styles/reset';
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <Routes />
+      <ResetCSS />
+    </>
+  );
 }
 
 export default App;
